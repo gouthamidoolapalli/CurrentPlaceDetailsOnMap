701136e94e69704caebcd82adb0c4d586d7c684c
gouthamidoolapalli <gouthami.doolapally@gmail.com>
2016-12-07 18:10:43 +0530

    set the map to satellite view

--- app/src/main/java/com/example/currentplacedetailsonmap/MapsActivityCurrentPlaces.java
+++ app/src/main/java/com/example/currentplacedetailsonmap/MapsActivityCurrentPlaces.java
@@ -61,7 +61,7 @@ public class MapsActivityCurrentPlaces extends AppCompatActivity implements
     // A default location (Sydney, Australia) and default zoom to use when location permission is
     // not granted.
     private final LatLng mDefaultLocation = new LatLng(-33.8523341, 151.2106085);
-    private static final int DEFAULT_ZOOM = 15;
+    private static final float DEFAULT_ZOOM = 19.0f;
     private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
     private boolean mLocationPermissionGranted;
 
@@ -173,6 +173,7 @@ public class MapsActivityCurrentPlaces extends AppCompatActivity implements
     @Override
     public void onMapReady(GoogleMap map) {
         mMap = map;
+        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
 
         // Turn on the My Location layer and the related control on the map.
         updateLocationUI();
